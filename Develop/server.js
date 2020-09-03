058a774ac15f0435c9e6a2538b9d23f678b29b57
RazzleJazzled <robertjgassner@gmail.com>
2020-09-03 09:57:32 -0700

    added changes

---
+++ Develop/server.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const express = require("express");
+const logger = require("morgan")
+const { v4: uuidv4 } = require("uuid")
+const app = express();
+const PORT = 8080;
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+app.get("/", function (req, res) {
+
+    res.sendFile(_dirname + "/public/index.html");
+
+});
+
+
+//View all notes
+app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
+
+        const notes = JSON.parse(data)
+
+        res.json(notes)
+    });
+});
+
+//POST - add new notes
+app.post("/api/notes", function (req, res) {
+    const note = {
+        id: uuidv4(),
+        ...req.body,
+    };
+
+    fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
+        const notes = JSON.parse(data);
+        notes.push(note);
+
+        const stringifiedData = JSON.stringify(notes, null, 2);
+
+        fs.writeFile(__dirname + "/db/db.json", stringifiedData, function () {
+            res.json(note);
+        });
+    });
+
+
+    res.json(note);
+});
+
+app.delete("/api/notes/:id", async function (req, res) {
+    try {
+    const { id } = req.params;
+    
+    const data = await fs.promises.readFile(_dirname + "/db/db.json", utf8);
+
+    let notes = JSON.parse(data);
+
+    notes = notes.filter((note) => note.id !== id);
+
+    const stringifiedData = JSON.stringify(notes, null, 2);
+
+    await fs.promises.writeFile(__dirname + "/db/db.json", stringifiedData)
+    } catch (err){
+        res.status(500).end();
+    }
+});
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
