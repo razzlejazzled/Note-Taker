77f9f384b706aba0041b9300a4092988d83bd9ba
RazzleJazzled <robertjgassner@gmail.com>
2020-09-04 14:53:32 -0700

    PORT Heroku compatible

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const logger = require("morgan")
 const { v4: uuidv4 } = require("uuid")
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
